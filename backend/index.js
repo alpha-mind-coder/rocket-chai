18216ae70cec8b10570a76ec4bf05db20f0e5411
alpha-mind-coder <aditikapoor123aditi@gmail.com>
2025-09-08 17:55:42 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -32,6 +32,7 @@ app.use((req, res, next) => {
   console.log("Session state:", req.session);
   next();
 });
+app.get("/healthz", (req, res) => res.send("OK"));
 
 // ✅ Homepage
 app.get("/", (req, res) => {
