36657401f3b7bc1bf51e2bc7d1afd8318ac15e17
alpha-mind-coder <aditikapoor123aditi@gmail.com>
2025-09-10 23:22:26 +0530

    Ignore backend files for Vercel

--- backend/index.js
+++ backend/index.js
@@ -32,6 +32,8 @@ app.use((req, res, next) => {
   console.log("Session state:", req.session);
   next();
 });
+app.get("/healthz", (req, res) => res.send("OK"));
+
 
 // ✅ Homepage
 app.get("/", (req, res) => {
